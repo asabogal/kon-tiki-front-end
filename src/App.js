f6280d213e9a0e8177626f87855b5ff8292647b7
asabogal <asabogal.git@gmail.com>
2019-07-10 23:13:31 -0400

    add Helmet to App for SEO optimization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Helmet from 'react-helmet'
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Kon-Tiki Greenport</title>
+          <meta name="description" content="Kon-Tiki Restaurant and Bar at The Gallery Hotel" />
+          <meta name="keywords" cpntent="greenport, restaurant, bar, gallery hotel, gallery, hotel" />
+        </Helmet>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
