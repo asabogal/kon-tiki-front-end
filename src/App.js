545f97c300659a71a575a009767ddbdfaffac133
asabogal <asabogal.git@gmail.com>
2019-07-10 23:24:48 -0400

    change font + add search keywords

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
         <Helmet>
           <title>Kon-Tiki Greenport</title>
           <meta name="description" content="Kon-Tiki Restaurant and Bar at The Gallery Hotel" />
-          <meta name="keywords" content="greenport, restaurant, bar, gallery hotel, gallery, hotel" />
+          <meta name="keywords" content="kon-tiki, kon, tiki, greenport, restaurant, bar, gallery hotel, gallery, hotel" />
         </Helmet>
+        <div>KON TIKI</div>
       </div>
     );
   }
