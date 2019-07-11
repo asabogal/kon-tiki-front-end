00e210e706b6e8325970a17eddbf9e12b1a805f5
asabogal <asabogal.git@gmail.com>
2019-07-11 02:38:56 -0400

    initial Header component + style

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './style.css'
 
+const styles = {
+  link: {
+    textDecoration: 'none',
+    color: 'black',
+  }
+}
+
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
+
   render() {
+       
     return (
       <div className="header-container">
-        <div> LOGO </div>
+        <div className="header-logo"> LOGO </div>
 
-        <ul className="header-nav">
-          <Link to="/">
-            <li>Location</li>
-          </Link>
-          <Link to="/">
-            <li>Location</li>
-          </Link>
-        </ul>
-
+      <ul className="header-nav">
+        <Link style={styles.link} to="/"> <li>WHERE | WHEN</li> </Link>
+        <Link style={styles.link} to="/"> <li>MENU</li> </Link>
+        <Link style={styles.link} to="/"> <li>GALLERY</li> </Link>
+        <Link style={styles.link} to="/"> <li>ABOUT</li> </Link>
+        <Link style={styles.link} to="/"> <li>PRESS</li> </Link>
+        <Link style={styles.link} to="/"> <li>RESERVE</li> </Link>
+      </ul>
+    
       </div>
     );
   }
