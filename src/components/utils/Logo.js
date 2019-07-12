942a8f3c58bb7277c4ebaf61e1ed3c9056e03c97
asabogal <asabogal.git@gmail.com>
2019-07-11 21:51:00 -0400

    finish Logo component + style

--- src/components/utils/Logo.js
+++ src/components/utils/Logo.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import logo from './images/logo.png'
+import './style.css'
 
 
 const Logo = (props) => {
 
-  const template = <div>LOGO!!</div>
+  const template = 
+    <div className="img-cover" 
+      style={{
+        width: props.width,
+        height: props.height,
+        background: `url(${logo}) no-repeat`
+      }}
+    >
+    </div>
   
 
   if(props.link) {
+    
     return (
-      <Link to={props.linkTo}>
+      <Link to={props.linkTo} className="link-logo">
         {template}
       </Link>
     )
 
   } else {
+
     return template
   }
  
