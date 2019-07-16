196ffdf43d379810979d4295698ba7ec445d86f2
asabogal <asabogal.git@gmail.com>
2019-07-16 00:51:13 -0400

    set fixed/sticky Footer

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -4,22 +4,26 @@ import './style.css'
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div>
-        <a className="footer-number"
-        href="tel: +1-631-477-4000"> 631-477-4000 </a>
+    <div className="footer-div">
+      <div className="footer-container">
+        <div>
+          <a className="footer-number"
+          href="tel: +1-631-477-4000"> 631-477-4000 </a>
+        </div>
+        <div>
+          <a className="footer-address" 
+          href="https://goo.gl/maps/VvUnSeM1wvvrmTYm7"> 437 Main Street - Greenport, NY 11944 </a>
+        </div>
+        <div>
+          <SocialBar
+            color="white"
+            size="2x"
+          />
+        </div>
       </div>
-      <div>
-        <a className="footer-address" 
-        href="https://goo.gl/maps/VvUnSeM1wvvrmTYm7"> 437 Main Street - Greenport, NY 11944 </a>
-      </div>
-      <div>
-        <SocialBar
-          color="white"
-          size="2x"
-        />
-      </div>
+
     </div>
+    
   );
 };
 
