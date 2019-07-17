14d29c7f8005bd702e75d7b7396f2d0de17c861a
asabogal <asabogal.git@gmail.com>
2019-07-16 23:43:38 -0400

    add Button rounter Link and better customization + style

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './style.css'
 
 const Button = (props) => {
-  return (
-    <button className="btn">
+
+  const template = 
+    <button className="btn"
+      style={{
+        fontSize: props.fontSize,
+        color: props.color,
+        padding: props.padding
+      }}
+    >
       {props.text}
     </button>
-  );
+
+  return (
+    <Link to={props.linkTo}>
+      {template}
+    </Link>
+  )
+  
 };
 
 export default Button;
