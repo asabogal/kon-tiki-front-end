7d26282b832542657488b6b7e8ae7acf2d7d069c
asabogal <asabogal.git@gmail.com>
2019-07-19 17:05:49 -0400

    setup image_slider container

---
+++ src/components/image_slider/Slider.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      images: [],
+      cIndex: 0
+     };
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Slider;
