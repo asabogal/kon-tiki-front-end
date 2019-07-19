7a47aa210187bcfaa763414e6f3c7e1e772f6fd2
asabogal <asabogal.git@gmail.com>
2019-07-19 18:33:54 -0400

    initial LandingBody + placeholder + style

---
+++ src/components/landing/LandingBody.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+
+const LandingBody = () => {
+  return (
+    <div className="landing-body">
+      <div>NAVIGATION </div>
+      <div>IMAGE</div>
+    </div>
+  );
+};
+
+export default LandingBody;
