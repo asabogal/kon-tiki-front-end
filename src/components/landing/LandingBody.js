86cbeeb38ef4cb86194905dcf1cc0be6fdc7a720
asabogal <asabogal.git@gmail.com>
2019-07-20 18:57:28 -0400

    initial LandingNavigation + style

--- src/components/landing/LandingBody.js
+++ src/components/landing/LandingBody.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import LandingImage from './LandingImage'
+import LandingNavigation from './LandingNavigation'
 import './style.css'
 
 const LandingBody = () => {
   return (
     <div className="landing-body">
-      <div>NAVIGATION </div>
+      <div><LandingNavigation/> </div>
       <div><LandingImage/></div>
     </div>
   );
---
+++ src/components/landing/LandingNavigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const LandingNavigation = () => {
+
+  return (
+    <ul className="landing-navigation">
+      <Link to="/home"> <li>HOME</li> </Link>
+      <Link to="/menu"> <li>MENU</li> </Link>
+      <Link to="/"> <li>RESERVE</li> </Link>
+      <Link to="/"> <li>CONTACT</li> </Link>
+    </ul>
+  );
+};
+
+export default LandingNavigation;
