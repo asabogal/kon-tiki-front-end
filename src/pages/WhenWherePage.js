9764cc65ea3e623784b1e33dbc90a4774d4ede19
asabogal <asabogal.git@gmail.com>
2019-07-31 02:25:01 -0400

    initial WhenWhere page

---
+++ src/pages/WhenWherePage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Head from '../components/when_where/Head'
+
+class WhenWherePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <div className="page-content">
+      <Head/>
+      </div>
+    );
+  }
+}
+
+export default WhenWherePage;
