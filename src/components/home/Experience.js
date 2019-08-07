792858fb12d1be9dd3c419d70fe642d63f0e700a
asabogal <asabogal.git@gmail.com>
2019-08-06 23:39:56 -0400

    add Experience section + style

--- src/components/home/Experience.js
+++ src/components/home/Experience.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Picture} from '../utils/Picture'
 import {Link} from 'react-router-dom'
+import rice from './images/friedrice.png'
+import boat from './images/kt_boat.png'
 import bar from './images/bar2.png'
+import eyes from './images/eyes.png'
+import hotel from './images/hotel.png'
 
 import './style.css'
 
@@ -12,27 +16,33 @@ const Experience = () => {
         <h1>EXPERIENCE</h1>
       </div>
       <div className="experience-gallery">
-       <div>
+       <div className="experience-thumbnail">
        Our Flavors
          <Link to="/menu">
           <Picture
-            image={bar}/>
+            image={rice}/>
          </Link>
        </div>
-       <div>
-       Our Journey
-         <Picture
-          image={bar}/>
+       <div className="experience-thumbnail">
+       Our Story
+       <Link to="/about">
+        <Picture
+          image={boat}/>
+       </Link>
        </div>
-       <div>
+       <div className="experience-thumbnail">
        Through Our Eyes
-         <Picture
-          image={bar}/>
+       <Link to="/gallery">
+        <Picture
+          image={eyes}/>
+       </Link>
        </div>
-       <div>
+       <div className="experience-thumbnail">
        The Hotel
+       <a href="http://galleryhotelny.com">
          <Picture
-          image={bar}/>
+          image={hotel}/>
+       </a>
        </div>
 
       </div>
