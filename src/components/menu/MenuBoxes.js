12867bf5dbdc9de25cbdf894de315a5bf3275bc9
asabogal <asabogal.git@gmail.com>
2019-08-08 17:27:39 -0400

    add initial MenuBoxes + style

---
+++ src/components/menu/MenuBoxes.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import TextBox from '../utils/TextBox'
+import Button from '../utils/Button'
+import {Picture} from '../utils/Picture'
+
+import dinner from './menu_pictures/friedrice.png'
+import drinks from './menu_pictures/KonTiki Signature Drink.jpg'
+import './style.css'
+
+const MenuBoxes = () => {
+
+  const dinnerContent = (
+    <p>
+      Service from 6pm - 11pm, Thu - Mon<br></br>
+      An intriguing mix of asian and latin american flavors.<br></br>
+      Carefully curated with local and imported produce.
+    </p>
+    );
+
+    const drinksContent = (
+      <p>
+        Bar open 'till late Thu - Mon<br></br>
+        Exotic, flavorful, and eccentric cocktails.<br></br>
+        Tropical flavors enriched with flare.
+      </p>
+      );
+
+  return (
+    <div className="boxes-wrapper">
+      <div className="box-container">
+        <div>
+        <TextBox
+          heading="DINNER"
+          content={dinnerContent}
+        />
+        <Button 
+          text="OPEN MENU" 
+          link={true} 
+          linkTo="/dinner"/>
+        </div>
+        <div>
+          <Picture  
+          image={dinner}
+          />
+        </div>
+
+      </div>
+
+      <div className="box-container">
+        <div>
+          <Picture  
+          image={drinks}
+          />
+        </div>
+        <div>
+        <TextBox
+            heading="DRINKS"
+            content={drinksContent}
+          />
+          <Button 
+            text="OPEN MENU" 
+            link={true} 
+            linkTo="/drinks"/>
+        </div>
+       
+      </div>
+      
+    </div>
+  );
+};
+
+export default MenuBoxes;
