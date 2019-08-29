74ee1ebb73622ed85e8ae90983fe23cef869d4d9
asabogal <asabogal.git@gmail.com>
2019-08-28 22:36:03 -0400

    initial FollowUs component and style

---
+++ src/components/home/FollowUs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import './style.css'
+
+
+const FollowUs = () => {
+  return (
+    <div className="followus-container"> 
+      <div>
+        <h1>
+          FOLLOW US ON INSTAGRAM
+        </h1>
+      </div>
+      <div className="icon-container">
+        <a href="https://www.instagram.com/kontiki.gp/">
+          <FontAwesomeIcon 
+            icon={faInstagram} 
+            size="8x"
+            color="white" 
+          />
+        </a>
+       
+      </div>
+    </div>
+  );
+};
+
+export default FollowUs;
