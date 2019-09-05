1c356e9dc8e9250c774aaa04ea60abb334ef9c94
asabogal <asabogal.git@gmail.com>
2019-09-05 13:12:26 -0400

    change intro text

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -6,8 +6,8 @@ const Intro = () => {
       <h3>WELCOME!</h3>
       <p>
         KonTiki is one of the North Fork’s most intriguing restaurants and bars.<br></br>
-	      Our unique approach to Asian cusine sets us apart from any culinary experience in the area.<br></br>
-	      Located at the beautiful Gallery Hotel, we welcome you to join us and experience it for yourself! Be it on our gorgeous backyard garden, or the cozy main room.<br></br>
+	      Spearheaded by Chef Cheo, our unique approach to cusine sets us apart from any culinary experience in the area.<br></br>
+	      Located at the beautiful Gallery Hotel in Greenport, NY, we welcome you to join us and experience it for yourself! Be it on our gorgeous backyard garden, or our cozy dinning room.<br></br>
       </p>
     </div>
   );
