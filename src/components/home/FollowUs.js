f0532aad7806e8e992238f812c61f116de1898e5
asabogal <asabogal.git@gmail.com>
2019-09-04 23:14:05 -0400

    initial Enjoy component + garden picture + styles

--- src/components/home/FollowUs.js
+++ src/components/home/FollowUs.js
@@ -8,15 +8,15 @@ const FollowUs = () => {
   return (
     <div className="followus-container"> 
       <div>
-        <h1>
+        {/* <h1>
           FOLLOW US ON INSTAGRAM
-        </h1>
+        </h1> */}
       </div>
       <div className="icon-container">
         <a href="https://www.instagram.com/kontiki.gp/">
           <FontAwesomeIcon 
             icon={faInstagram} 
-            size="8x"
+            size="6x"
             color="white" 
           />
         </a>
