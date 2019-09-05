a4368d18f8d58196049b2cdea0c7546dc65436c0
asabogal <asabogal.git@gmail.com>
2019-09-05 18:02:05 -0400

    better styling

--- src/components/home/SubHero.js
+++ src/components/home/SubHero.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade'
+import Button from '../utils/Button'
 import image from './images/entrance.png'
 import './style.css'
+
+import Radium from 'radium'
 
 const SubHero = () => {
 
@@ -11,28 +14,59 @@ const SubHero = () => {
       gridTemplateColumns: '1fr 1fr',
       background: `url(${image}) no-repeat`,
       backgroundPosition: 'center center',
-      backgroundSize: 'cover'
+      backgroundSize: 'cover',
+      '@media (max-width: 768px)': {
+        gridTemplateRows: '1fr 1fr',
+        gridTemplateColumns: '1fr',
+      }
+    }, 
+    content: {
+      color: 'rgb(198, 245, 248)',
+      fontSize: '18px',
+      fontWeight: '500',
+      justifySelf: 'center',
+      '@media (max-width: 768px)': {
+        justifySelf: 'center',
+        alignSelf: "center",
+        margin: '0 auto'
+      }
     }
   }
 
   const template = 
     <Fade distance="25px" bottom>
-      <div>
-
-      </div>
       <div className="subhero" style={styles.hero}>
-        <div className="subhero-left">
-              CONTACT US
+        <div style={styles.content}>
+          <h4>GET IN TOUCH</h4>
+          <p>Get in touch with us. We'd love to hear from you.</p>
+          <Button
+            text="CONTACT US"
+            color="black"
+            fontSize='17px'
+            padding='5px 40px'
+            background='rgb(198, 245, 248)'
+            link={true}
+            linkTo="/contact"
+            />
         </div>
-        <div className="subhero-left">
-              RESERVE
+        <div style={styles.content}>
+          <h4>RESERVE A TABLE</h4>
+          <p>Eat and enjoy together.</p>
+          <Button
+            text="RESERVE"
+            color="black"
+            fontSize='17px'
+            padding='5px 40px'
+            background='rgb(198, 245, 248)'
+            link={true}
+            linkTo="/reserve"
+            />
         </div>
        </div>
     </Fade>
     
-      
   return template
 
 };
 
-export default SubHero;
+export default Radium(SubHero);
